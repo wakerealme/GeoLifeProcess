5369cd1b13908e3c667a6d23481a161f23adecec
wakerealme <139942776+wakerealme@users.noreply.github.com>
2023-07-19 18:40:28 +0800

    Update GoogleMap.py

--- GoogleMap.py
+++ GoogleMap.py
@@ -7,26 +7,34 @@ def isContainsChinese(strs):
             return True
     return False
 
-
-def searchPlaces(gmaps, lat, lng, radius=1000, place_type='bar'):
+def searchPlaces(gmaps, lat, lng, radius=500):
     places = []
-    search_results = gmaps.places_nearby(location=(lat, lng), radius=radius, type=place_type, language='en')
+    search_results = gmaps.places_nearby(location=(lat, lng), radius=radius, language='en')
     if 'results' in search_results:
         for result in search_results['results']:
             name = result['name']
+            type = result['types']
             location = result['geometry']['location']
             if(isContainsChinese(name)==False):
-                places.append({'name': name, 'latitude': location['lat'], 'longitude': location['lng']})
-                print(f"Name: {name}, Latitude: {location['lat']}, Longitude: {location['lng']}")
+                places.append({'type': type[0], 'name': name,  'latitude': location['lat'], 'longitude': location['lng']})
+                print(f"Type:{type}, Name: {name}, Latitude: {location['lat']}, Longitude: {location['lng']}")
     return places
 
 
+
+
 if __name__ == '__main__':
-    api_key = 'YOUR_GOOGLE_API_KEY'
+    api_key = 'AIzaSyAGiiAczI3P5gKeyHiGOsZ_Mp8AcM2ZbVc'
     gmaps = googlemaps.Client(key=api_key)
-    stayPoint={"longitude": 116.29573975, "latitude": 39.98693575, "userid": "0"}
+    stayPoint={"longitude": 116.29573975, "latitude": 39.78693575, "userid": "0"}
     radius = 1000
-    placeType = 'school'
-    #place types: https://developers.google.com/maps/documentation/places/web-service/supported_types
-    placesInfo = searchPlaces(gmaps, stayPoint['latitude'], stayPoint['longitude'], 1000, placeType)
-    print(len(placesInfo))
+    # place types: https://developers.google.com/maps/documentation/places/web-service/supported_types
+    placesInfo = searchPlaces(gmaps, stayPoint['latitude'], stayPoint['longitude'])
+
+
+
+
+
+
+
+
